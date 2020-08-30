2d4df57bb203722c06d75cd3444bab42838f81bb
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2020-08-30 08:22:37 +0700

    add backdrop for <Filter /> to handleClose (done)

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -7,19 +7,26 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import ButtonBase from "@material-ui/core/ButtonBase";
+import Backdrop from "@material-ui/core/Backdrop";
 
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 200,
+    position: "relative",
   },
   select: {
     display: "flex",
     alignItems: "center",
-    padding: [16, 16, 16, 24],
     marginBottom: 4,
+    position: "relative",
+    zIndex: 12,
+    "& button": {
+      padding: [16, 16, 16, 24],
+      width: "100%",
+    },
     "& p": {
-      fontSize: ".725rem",
+      fontSize: ".75rem",
       fontWeight: 600,
     },
     "& svg": {
@@ -27,11 +34,32 @@ const useStyles = makeStyles((theme) => ({
       fontSize: "1rem",
     },
   },
-  menu: {},
+  menu: {
+    padding: [16, 16, 16, 24],
+    position: "absolute",
+    zIndex: 11,
+    "& button": {
+      width: "100%",
+      justifyContent: "start",
+      padding: [2, 0],
+      "& p": {
+        fontSize: ".75rem",
+        fontWeight: 600,
+      },
+    },
+  },
+  backdrop: {
+    zIndex: 10,
+    backgroundColor: "transparent",
+  },
 }));
 // colorScheme == "dark" ? "dark-mode" : null
 function Filter({ className, colorScheme }) {
+  const [open, setOpen] = React.useState(false);
   const styles = useStyles();
+  function handleClose() {
+    setOpen(false);
+  }
   return (
     <div className={clsx(styles.root, className)}>
       <Paper
@@ -41,32 +69,44 @@ function Filter({ className, colorScheme }) {
         )}
         elevation={4}
       >
-        <Typography>Filter By Region</Typography>
-        <ExpandMoreIcon />
+        <ButtonBase onClick={() => setOpen((open) => !open)}>
+          <Typography>Filter By Region</Typography>
+          <ExpandMoreIcon />
+        </ButtonBase>
       </Paper>
-      <Paper
-        className={clsx(
-          styles.menu,
-          colorScheme == "dark" ? "dark-mode" : null
-        )}
-        elevation={4}
-      >
-        <FilterItem>Africa</FilterItem>
-        <FilterItem>America</FilterItem>
-        <FilterItem>Asia</FilterItem>
-        <FilterItem>Europe</FilterItem>
-        <FilterItem>Oceania</FilterItem>
-      </Paper>
+      {open && (
+        <>
+          <Backdrop
+            className={styles.backdrop}
+            open={open}
+            onClick={handleClose}
+          />
+          <Paper
+            className={clsx(
+              styles.menu,
+              colorScheme == "dark" ? "dark-mode" : null
+            )}
+            elevation={4}
+          >
+            <FilterItem>Africa</FilterItem>
+            <FilterItem>America</FilterItem>
+            <FilterItem>Asia</FilterItem>
+            <FilterItem>Europe</FilterItem>
+            <FilterItem>Oceania</FilterItem>
+          </Paper>
+        </>
+      )}
     </div>
   );
 }
 function FilterItem({ className, children }) {
   return (
-    <ButtonBase>
+    <ButtonBase className={clsx(className)}>
       <Typography>{children}</Typography>
     </ButtonBase>
   );
 }
+
 function mapState(state) {
   return {
     colorScheme: state.colorScheme,
