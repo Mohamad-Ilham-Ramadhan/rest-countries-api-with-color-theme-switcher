cf4a62062a5767882d821992962e22921e033d83
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2020-10-15 12:23:00 +0700

    add Map using leaflet and react-leaflet

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -8,7 +8,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
-
+import Map from "./Map";
 import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 
 import flag from "../images/german-flag.svg";
@@ -140,6 +140,10 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
+  coba: {
+    backgroundColor: "tomato",
+    height: 536,
+  },
 }));
 
 function Detail({
@@ -157,7 +161,6 @@ function Detail({
       getCountryDetail(params.name, countries);
     }
   }, [countries]);
-
   return (
     <>
       {loadingCountryDetail === true ? (
@@ -270,6 +273,7 @@ function Detail({
               </Grid>
             </Grid>
           </Grid>
+          <Map location={countryDetail.latlng} />
         </section>
       )}
     </>
---
+++ src/components/Map.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import L from "leaflet";
+import { Map, TileLayer, Marker } from "react-leaflet";
+import locationIcon from "../images/icon-location.svg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: 535,
+    zIndex: 50,
+  },
+}));
+
+const markerIcon = L.icon({
+  iconUrl: locationIcon,
+  iconSize: [46, 56],
+});
+
+export default function Mapsdf({ className, location }) {
+  const styles = useStyles();
+  console.log("location =>", location);
+  return (
+    <Map
+      className={styles.root}
+      center={location}
+      zoom={6}
+      // zoomControl={false}
+    >
+      <TileLayer
+        url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+        maxZoom={18}
+        id="mapbox/streets-v11"
+        tileSize={512}
+        zoomOffset={-1}
+        accessToken="pk.eyJ1IjoibS1pbGhhbS1yIiwiYSI6ImNrZ2FheXhiczA1ZGYyc3FjbW1yNWY4NG8ifQ.YH_QmdJYa7Xp807y7CpV2w"
+      />
+      <Marker icon={markerIcon} position={location}></Marker>
+    </Map>
+  );
+}
